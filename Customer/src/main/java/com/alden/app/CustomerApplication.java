6691bd6c60bd020c93fd382f74aa808af6627d10
trung vu <vuvantrung002@gmail.com>
2024-10-06 00:08:40 +0700

    init project

---
+++ Customer/src/main/java/com/alden/app/CustomerApplication.java
@@ -0,0 +1,16 @@
+package com.alden.app;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.alden.clients")
+public class CustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
+}
